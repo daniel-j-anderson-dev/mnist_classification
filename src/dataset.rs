46bc57f78bb664e66a9cdd5b7e04739b96d7ad33
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-11-19 19:10:13 -0700

    created helper functions to get image labels

--- src/dataset.rs
+++ src/dataset.rs
@@ -9,6 +9,8 @@ pub const TRAINING_IMAGE_COUNT: usize = 60000;
 pub const TEST_IMAGE_COUNT: usize = 10000;
 
 pub const IMAGE_DATA_OFFSET: usize = 16;
+pub const IMAGE_LABEL_OFFSET: usize = 8;
+
 pub const IMAGE_WIDTH: usize = 28;
 pub const IMAGE_HEIGHT: usize = 28;
 pub const IMAGE_SIZE: usize = IMAGE_WIDTH * IMAGE_HEIGHT;
@@ -46,13 +48,35 @@ pub fn get_test_image(image_index: usize) -> &'static [u8; IMAGE_SIZE] {
         .expect("image_range.len() == IMAGE_SIZE")
 }
 
+pub fn get_training_label(image_index: usize) -> u8 {
+    assert!(image_index < TRAINING_IMAGE_COUNT);
+    TRAINING_LABELS[image_index + IMAGE_LABEL_OFFSET]
+}
+
+pub fn get_test_label(image_index: usize) -> u8 {
+    assert!(image_index < TEST_IMAGE_COUNT);
+    TEST_LABELS[image_index + IMAGE_LABEL_OFFSET]
+}
+
 #[cfg(test)]
 mod test {
     use crate::dataset::{
-        get_test_image, get_training_image, IMAGES_MAGIC_NUMBER, IMAGE_HEIGHT, IMAGE_WIDTH,
-        LABELS_MAGIC_NUMBER, TEST_IMAGES, TEST_IMAGE_COUNT, TEST_LABELS, TRAINING_IMAGES,
-        TRAINING_IMAGE_COUNT, TRAINING_LABELS,
+        get_test_image, get_test_label, get_training_image, get_training_label,
+        IMAGES_MAGIC_NUMBER, IMAGE_HEIGHT, IMAGE_WIDTH, LABELS_MAGIC_NUMBER, TEST_IMAGES,
+        TEST_IMAGE_COUNT, TEST_LABELS, TRAINING_IMAGES, TRAINING_IMAGE_COUNT, TRAINING_LABELS,
     };
+
+    #[test]
+    fn test_get_training_label() {
+        assert_eq!(get_training_label(0), 5);
+        assert_eq!(get_training_label(59999), 8);
+    }
+    
+    #[test]
+    fn test_get_test_label() {
+        assert_eq!(get_test_label(0), 7);
+        assert_eq!(get_test_label(9999), 6);
+    }
 
     #[test]
     fn test_get_training_image() {
