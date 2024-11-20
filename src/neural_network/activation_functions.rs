1abccd5a268de1bab9e191069565d0f4eec0a9f8
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-11-20 02:06:17 -0700

    made a trait for ReLu

--- src/neural_network/activation_functions.rs
+++ src/neural_network/activation_functions.rs
@@ -1,7 +1,5 @@
-pub fn rectified_linear_unit(mut z: impl AsMut<[f64]>) {
-    for z_i in z.as_mut().iter_mut() {
-        *z_i = z_i.max(0.0);
-    }
+pub fn rectified_linear_unit(x: f64) -> f64 {
+    x.max(0.0)
 }
 
 pub fn soft_max(mut z: impl AsMut<[f64]>) {
@@ -11,5 +9,17 @@ pub fn soft_max(mut z: impl AsMut<[f64]>) {
 
     for z_i in z.iter_mut() {
         *z_i /= sum;
+    }
+}
+
+pub trait SoftMax {
+    fn soft_max(&mut self);
+}
+impl<Arr> SoftMax for Arr
+where
+    Arr: AsMut<[f64]>,
+{
+    fn soft_max(&mut self) {
+        soft_max(self);
     }
 }
