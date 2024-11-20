a29303bc0293a564df8998b2fc3259b2589881e7
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-11-20 02:07:02 -0700

    implemented forward propagation

--- src/neural_network.rs
+++ src/neural_network.rs
@@ -1,15 +1,23 @@
+use activation_functions::{rectified_linear_unit, soft_max, SoftMax};
+
 pub mod activation_functions;
 
 #[derive(Debug, Clone)]
 pub struct NeuralNetwork {
     layers: Box<[Layer]>,
+    input_node_count: usize,
 }
 impl NeuralNetwork {
+    /// # Panics
+    /// if `layer_sizes` is empty or any of the layer sizes are 0
     pub fn new(layer_sizes: impl AsRef<[usize]>) -> Self {
         use rand::Rng;
-        let mut rng = rand::thread_rng();
 
         let layer_sizes = layer_sizes.as_ref();
+        assert!(layer_sizes.len() != 0);
+        assert!(layer_sizes.iter().any(|&layer_size| layer_size != 0));
+
+        let mut rng = rand::thread_rng();
 
         let layers = (1..layer_sizes.len())
             .map(|layer_index| {
@@ -36,12 +44,38 @@ impl NeuralNetwork {
             })
             .collect();
 
-        NeuralNetwork { layers }
+        NeuralNetwork {
+            layers,
+            input_node_count: layer_sizes[0],
+        }
     }
-    pub fn forward_pass(&mut self, inputs: impl AsRef<[f64]>) {
-        unimplemented!()
+
+    pub fn forward_propagation(&mut self, inputs: impl AsRef<[f64]>) {
+        let mut activations = inputs.as_ref();
+        assert!(activations.len() == self.input_node_count);
+
+        for layer in self.layers.iter_mut() {
+            for (node_index, (weights, bias)) in layer.weights.iter().zip(&layer.biases).enumerate()
+            {
+                let weighted_sum: f64 = weights
+                    .iter()
+                    .zip(activations)
+                    .map(|(weight, activation)| weight * activation)
+                    .sum();
+                layer.activations[node_index] = rectified_linear_unit(weighted_sum + bias);
+            }
+
+            activations = &layer.activations;
+        }
+
+        self.layers
+            .last_mut()
+            .expect("NeuralNetwork::new would panic if there isn't at least 1 layer")
+            .activations
+            .soft_max();
     }
-    pub fn backward_pass(&mut self) {
+
+    pub fn backward_propagation(&mut self) {
         unimplemented!()
     }
 }
@@ -61,5 +95,16 @@ impl Layer {
 
 #[cfg(test)]
 mod test {
-    use crate::neural_network::{Layer, NeuralNetwork};
+    use crate::{
+        dataset::IMAGE_SIZE,
+        neural_network::{Layer, NeuralNetwork},
+    };
+
+    #[test]
+    fn f() {
+        let nn = NeuralNetwork::new([IMAGE_SIZE, 10, 10]);
+        for l in nn.layers.iter() {
+            println!("{:?}", l);
+        }
+    }
 }
