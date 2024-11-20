599666b1738e348abddf33e02c6bbf340a1c3e9b
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-11-20 12:58:29 -0700

    implemented back backward propagation

--- src/neural_network.rs
+++ src/neural_network.rs
@@ -1,4 +1,4 @@
-use activation_functions::{rectified_linear_unit, soft_max, SoftMax};
+use activation_functions::{rectified_linear_unit, rectified_linear_unit_derivative, SoftMax};
 
 pub mod activation_functions;
 
@@ -24,9 +24,9 @@ impl NeuralNetwork {
                 Layer {
                     // generate a random weight for every combination of node indexes between the current row and the previous row
                     weights: (0..layer_sizes[layer_index])
-                        .map(|_current_layer_node_index| {
+                        .map(|_current_layer_neuron_index| {
                             (0..layer_sizes[layer_index - 1])
-                                .map(|_previous_layer_node_index| rng.gen_range(-1.0..1.0))
+                                .map(|_previous_layer_neuron_index| rng.gen_range(-1.0..1.0))
                                 .collect()
                         })
                         .collect(),
@@ -50,33 +50,118 @@ impl NeuralNetwork {
         }
     }
 
-    pub fn forward_propagation(&mut self, inputs: impl AsRef<[f64]>) {
-        let mut activations = inputs.as_ref();
-        assert!(activations.len() == self.input_node_count);
-
-        for layer in self.layers.iter_mut() {
-            for (node_index, (weights, bias)) in layer.weights.iter().zip(&layer.biases).enumerate()
-            {
-                let weighted_sum: f64 = weights
-                    .iter()
-                    .zip(activations)
-                    .map(|(weight, activation)| weight * activation)
-                    .sum();
-                layer.activations[node_index] = rectified_linear_unit(weighted_sum + bias);
-            }
-
-            // pass activations to the next layer
-            activations = &layer.activations;
-        }
-
+    fn last_layer(&self) -> &Layer {
+        self.layers
+            .last()
+            .expect("NeuralNetwork::new would panic if there isn't at least 1 layer")
+    }
+    fn last_layer_mut(&mut self) -> &mut Layer {
         self.layers
             .last_mut()
             .expect("NeuralNetwork::new would panic if there isn't at least 1 layer")
-            .activations
-            .soft_max();
     }
 
-    pub fn backward_propagation(&mut self) {
+    pub fn forward_propagation(&mut self, inputs: impl AsRef<[f64]>) {
+        let mut previous_layer_activations = inputs.as_ref();
+        assert!(
+            previous_layer_activations.len() == self.input_node_count,
+            "there must be exactly self.input_node_count inputs"
+        );
+
+        for layer in self.layers.iter_mut() {
+            for (neuron_index, (weights, bias)) in
+                layer.weights.iter().zip(&layer.biases).enumerate()
+            {
+                let weighted_sum: f64 = weights
+                    .iter()
+                    .zip(previous_layer_activations)
+                    .map(|(weight, activation)| weight * activation)
+                    .sum();
+                layer.activations[neuron_index] = rectified_linear_unit(weighted_sum + bias);
+            }
+
+            // pass activations to the next layer
+            previous_layer_activations = &layer.activations;
+        }
+
+        self.last_layer_mut().activations.soft_max();
+    }
+
+    pub fn backward_propagation(
+        &mut self,
+        targets: impl AsRef<[f64]>,
+        learning_rate: f64,
+    ) {
+        let targets = targets.as_ref();
+
+        assert_eq!(
+            targets.len(),
+            self.last_layer().activations.len(),
+            "the number of targets must be equal to the number of neurons in the final layer"
+        );
+
+        // compute error
+        let mut deltas = self
+            .last_layer()
+            .activations
+            .iter()
+            .zip(targets)
+            .map(|(output, target)| output - target)
+            .collect::<Box<_>>();
+
+        // back propagate through each layer
+        for layer_index in (0..self.layers.len()).rev() {
+            {
+                let layer = &mut self.layers[layer_index];
+
+                // calculate weights and biases gradients
+                let weight_gradients = deltas
+                    .iter()
+                    .map(|delta| {
+                        layer
+                            .activations
+                            .iter()
+                            .map(|activation| delta * activation)
+                            .collect::<Box<_>>()
+                    })
+                    .collect::<Box<_>>();
+
+                let bias_gradients = deltas.clone();
+
+                // update weights and biases according to their gradients
+                for (weights, weight_gradients) in layer.weights.iter_mut().zip(weight_gradients) {
+                    for (weight, weight_gradient) in weights.iter_mut().zip(weight_gradients) {
+                        *weight -= learning_rate * weight_gradient;
+                    }
+                }
+
+                for (bias, bias_gradient) in layer.biases.iter_mut().zip(bias_gradients) {
+                    *bias -= learning_rate * bias_gradient;
+                }
+            }
+
+            // calculate error for previous layer
+            if layer_index > 0 {
+                let layer = &self.layers[layer_index];
+                let previous_layer = &self.layers[layer_index - 1];
+
+                deltas = (0..previous_layer.activations.len())
+                    .map(|previous_layer_neuron_index| {
+                        layer
+                            .weights
+                            .iter()
+                            .zip(&deltas)
+                            .fold(0.0, |sum, (weights, delta)| {
+                                sum + weights[previous_layer_neuron_index] * delta
+                            })
+                            * rectified_linear_unit_derivative(
+                                previous_layer.activations[previous_layer_neuron_index],
+                            )
+                    })
+                    .collect();
+            }
+        }
+
         unimplemented!()
     }
 }
