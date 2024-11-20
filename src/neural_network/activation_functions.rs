da7cac7c6dcc3d47da9a7e13c952b73c6b821abb
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-11-20 00:41:15 -0700

    added soft max

---
+++ src/neural_network/activation_functions.rs
@@ -0,0 +1,13 @@
+pub fn rectified_linear_unit(x: f64) -> f64 {
+    x.max(0.0)
+}
+
+pub fn soft_max(mut z: impl AsMut<[f64]>) {
+    let z = z.as_mut();
+
+    let sum = z.iter().map(|z_i| z_i.exp()).sum::<f64>();
+
+    for z_i in z.iter_mut() {
+        *z_i /= sum;
+    }
+}
