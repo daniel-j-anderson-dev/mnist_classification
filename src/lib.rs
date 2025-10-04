938fc018c6db1d930104aa3356a9ced6952137c6
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2025-10-03 20:02:09 -0600

    moved one_hot_encode to DigitClass

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,5 @@
 pub mod dataset;
 pub mod neural_network;
 
-pub fn one_hot_encode(label: usize) -> [f64; 10] {
-    assert!(label < 10, "there are only ten digits");
-    let mut encoded = [0.0; 10];
-    encoded[label] = 1.0;
-    encoded
-}
+pub use dataset::*;
+
